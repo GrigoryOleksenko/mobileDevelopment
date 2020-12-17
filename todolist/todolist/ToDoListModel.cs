713159d58724a591d705e84e909f1dd4d4147b02
GrigoryOleksenko <76180009+GrigoryOleksenko@users.noreply.github.com>
2020-12-17 23:49:31 +0500

    Add files via upload
    Первая лабораторная работа

---
+++ todolist/todolist/ToDoListModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using todolist.Annotations;
+
+namespace todolist
+{
+   public class ToDoListModel : INotifyPropertyChanged
+    {
+        public ToDoListModel()
+        {
+            ToDoItems = new ObservableCollection<object>();
+        }
+
+        public void AddValue(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+            ToDoItems.Add(value);
+        }
+
+        public void RemoveValue(object str)
+        {
+           ToDoItems.Remove(str);
+        }
+
+        public ObservableCollection<object> ToDoItems { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ todolist/todolist/ToDoListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using todolist.Annotations;
+using Xamarin.Forms;
+
+namespace todolist
+{
+    public class ToDoListViewModel : INotifyPropertyChanged
+    {
+        readonly ToDoListModel _model = new ToDoListModel();
+
+        public ToDoListViewModel()
+        {
+            _model.PropertyChanged += (s, e) =>
+            {
+                OnPropertyChanged(e.PropertyName);
+            };
+
+            Delete = new Command<object>(i =>
+            {
+                _model.RemoveValue(i);
+            });
+
+            Add = new Command<string>(str => {
+               _model.AddValue(str);
+               InputText = string.Empty;
+               OnPropertyChanged("InputText");
+               Console.Write(MyValues);
+            });
+            MyValues.Add("qwdqwd");
+        }
+
+        public Command<string> Add { get; set; }
+        public Command<object> Delete { get; set; }
+        public ObservableCollection<object> MyValues => _model.ToDoItems;
+        public string InputText { get; set; }
+        public object ItemSelected { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
